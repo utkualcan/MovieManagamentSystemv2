d5c28446913bfd70f091a314a6c657485a4bb3de
Utku Alcan <aalcan.utku@gmail.com>
2024-05-18 17:50:53 +0300

    bubbles

--- src/main/java/org/kurgu/moviemanagamentsystemv2/Security/SecurityConfig.java
+++ src/main/java/org/kurgu/moviemanagamentsystemv2/Security/SecurityConfig.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +15,8 @@ public class SecurityConfig {
     private UserDetailsServiceDAO userDetailsService;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
